8e57c0ac7cc4c7488129c3ef2129f6b78e806d0d
ori kerbis <orikerbis@gmail.com>
2024-08-20 22:32:30 +0300

    first

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+import unittest
+from app import app
+
+class BasicTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"message": "Hello, World!"})
+
+    def test_add(self):
+        response = self.client.get('/add/2/3')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"result": 5})
+
+        response = self.client.get('/add/10/20')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"result": 30})
+
+if __name__ == "__main__":
+    unittest.main()
