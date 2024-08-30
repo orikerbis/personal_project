0a29f270bc25d115e68820e7d62ab861e2eecc48
orikerbis <164182077+orikerbis@users.noreply.github.com>
2024-08-30 16:32:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hellog, Wrorld ")
+    return jsonify(message="Hello, Wrorld ")
 
 @app.route('/add/<int:a>/<int:b>')
 def add(a, b):
